2c8487e0ca460e2cc10abaddee2ea7499bebaf84
Drew Price <drewprice@users.noreply.github.com>
2015-10-10 16:34:09 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready( function() {
+	$('a.el-btn').click(function(e) {
+	    e.preventDefault();
+	    window.open('https://xh.io/');
+	    window.open('http://the-lineup.launchrock.com/');
+	    window.open('https://www.possefoundation.org/');
+	    window.open('http://metro.org/');
+	    window.open('https://ckmadvisors.com/');
+	    window.open('http://www.sourcemap.com/');
+	    window.open('http://www.bounceexchange.com/');
+	    window.open('http://home.crowdtap.com/');
+	    window.open('https://www.jopwell.com/');
+	    window.open('http://www.bighuman.com/#/work/');
+	    window.open('http://www.greenhouse.io/');
+	    window.open('http://architizer.com/');
+	    window.open('https://www.plated.com/');
+	    window.open('https://teachboost.com/');
+	    window.open('https://www.beenverified.com/');
+	    window.open('http://nymag.com/');
+	    window.open('http://www.wk.com/');
+	    window.open('http://vaynermedia.com/');
+	});
+});
